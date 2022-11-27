65038ae5b9d8813fcd7eb173c7207edc4e1a0e78
MoNaSser8 <11410120200479@stud.cu.edu.eg>
2022-11-27 16:17:15 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String arg[]){
+
+        Scanner cin = new Scanner(System.in);
+        int x=cin.nextInt();
+        System.out.println(x);
+    }
+}
